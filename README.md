634e56e3a9562a18e98e81f5208b056e5735c03d
Ralph <129913847+LopezAnthonyRalph@users.noreply.github.com>
2023-08-22 13:32:50 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# TryBranchXRalph
+# TryBranch X Ralph
+
+TryBranch is a link in bio tool built using [Astro]
+
+[TryBranch Team] (Aug 22nd - Present)
+
+- Ralph Lopez (Developer & UI/UX Designer)
+
+
+
+
+View the [Demo Site]()
---
+++ src/links.js
@@ -0,0 +1,50 @@
+export const mainContent = {
+  imgSrc: `/assets/ralph.png`,
+  name: `@ Ralph`,
+  tagline: ``,
+};
+
+
+
+export const socialLinks = [
+  {
+    title: 'Linkedin',
+    icon: 'mdi:linkedin',
+    url: '',
+  },
+  {
+    title: 'Github',
+    icon: 'mdi:github  ',
+    url: '',
+  },
+  {
+    title: 'Instagram',
+    icon: 'mdi:instagram  ',
+    url: '',
+  },
+  {
+    title: 'discord',
+    icon: 'mdi:discord',
+    url: '',
+  }
+]
+
+export const links = [
+  {
+    text: 'Home',
+    url: '',
+    featured: true,
+  },
+  {
+    text: 'About',
+    url: '',
+  },
+  {
+    text: 'Uses',
+    url: '',
+  },
+  {
+    text: 'See my latest blog post!',
+    url: '',
+  }
+]
